b4874cd29f04725d1da5d0c652d1244098ca3876
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-09-04 12:44:17 +0700

    Remove Profile Seeder and Update README.md

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $profile = new Profile;
-
-        $profile->member_id         = '220210001';
-        $profile->student_number    = '2038201057';
-        $profile->batch             = '2020';
-        $profile->faculty           = 'VOCATION';
-        $profile->major             = 'Industrial Mechanical Engineering';
-        $profile->phone             = '081615061316';
-        $profile->line_id           = 'rizram2001';
-
-        $profile->save();
-
         $user = new User;
 
         $user->name     = 'Daffa Kurnia Fatah';
