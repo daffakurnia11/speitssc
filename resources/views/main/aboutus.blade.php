733cf40a5e2141ba63dbeebeb555bcab40fdebe8
argyax <78781899+argyax@users.noreply.github.com>
2021-09-13 17:12:05 +0800

    Update aboutus.blade.php

--- resources/views/main/aboutus.blade.php
+++ resources/views/main/aboutus.blade.php
@@ -67,12 +67,14 @@
             <h2 class="top-wrapper-title">Mission <hr class="tw-line"></h2>
             </p>
             <p class="header-desc fs-5 row-aligns-item-right">
-              1. Facilitate and enhance a complete comprehension and exposure of the oil and gas industry, serving as anaccurate career preparation for members and boards.<br>
-              2. Stimulate personal development through organizational experience that polishes professionalism and competence.<br>
-              3. Guide and facilitate our delegates in preparation of oil and gas related competitions locally and internationally.<br>
-              4. Improve relations with companies, stakeholders, communities, SPE International, SPE Java, and other SPE Student Chapters.<br>
-              5. Create and maintain a favorable and collaborative working environment that is conducive to progress and productivity.<br>
-              6. Increase community involvement through conducting social responsibilities.<br>
+              <ol class="header-desc">
+                <li>Facilitate and enhance a complete comprehension and exposure of the oil and gas industry, serving as anaccurate career preparation for members and boards.</li>
+                <li>Stimulate personal development through organizational experience that polishes professionalism and competence.</li>
+                <li>Guide and facilitate our delegates in preparation of oil and gas related competitions locally and internationally.</li>
+                <li>Improve relations with companies, stakeholders, communities, SPE International, SPE Java, and other SPE Student Chapters.</li>
+                <li>Create and maintain a favorable and collaborative working environment that is conducive to progress and productivity.</li>
+                <li>Increase community involvement through conducting social responsibilities.</li>
+              </ol>
             </p>
           </div>
         </div>
@@ -210,81 +212,6 @@
     </div>
 
   {{--End of Sectionn--}}
-
-
-
-            {{--
-            <div class="box">
-              <div class="left-box">
-                <img src="img/achievement.png">
-                <p>SPE ITS Student Chapter has earned the Gold Standard Designation for 2015, in Recognition of its  exceptional programs in Industry Engagement, Operations and Planning, Community Involvement,  Professional Development and Innovation</p>
-              </div>
-                <p>SPE Gold Standard Designation 2015, SPE International, June 2015<p>
-            </div>
-            
-
-            <div class="v5_541">
-              <div class="v5_482"></div>
-              <div class="v5_483"></div>
-              <div class="v5_486"></div>
-              <div class="v5_487"></div>
-              <span class="v5_488">SPE ITS Student Chapter has earned APSC Event, which was the first time in Indonesia</span>
-              <span class="v5_489">SPE Asia Pacific SPE Student Conference 2016</span>
-              <div class="name"></div>
-            </div>
-
-            <div class="v5_543">
-              <div class="v5_492"></div>
-              <div class="v5_491"></div>
-              <div class="v5_495"></div>
-              <div class="v5_496"></div>
-              <span class="v5_497">SPE ITS Student Chapter has earned the Gold Standard Designation for 2016, in Recognition of its  exceptional programs in Industry Engagement, Operations and Planning, Community Involvement,  Professional Development and Innovation</span>
-              <span class="v5_498">SPE Gold Standard Designation 2016, SPE International. June 2016</span>
-              <div class="name"></div>
-            </div>
-
-            <div class="v5_546">
-              <div class="v5_500"></div>
-                <div class="v5_501"></div>
-                <div class="v5_504"></div>
-                <div class="v5_505"></div>
-                <span class="v5_506">SPE ITS Student Chapter has earned the Outstanding Student Chapter Award for 2017, in  Recognition of its exceptional programs in Industry Engagement, Operations and  Planning, Communinty Involvement, Professional Development and Innovation</span>
-                <span class="v5_507">SPE Outstanding Student Chapter (OSC) Award, SPE International. June 2017</span>
-                <div class="name"></div>
-              </div>
-
-              <div class="v5_540">
-                <div class="v5_509"></div>
-                <div class="v5_510"></div>
-                <div class="v5_513"></div>
-                <div class="v5_514"></div>
-                <span class="v5_515">SPE ITS Student Chapter has earned the privilege to host the APSC Event,  which is the only student chapter in Indonesia that have done it</span>
-                <span class="v5_516">SPE Asia Pacific SPE Student Conference 2018</span>
-                <div class="name"></div>
-              </div>
-
-              <div class="v5_542">
-                <div class="v5_518"></div>
-                <div class="v5_519"></div>
-                <div class="v5_522"></div>
-                <div class="v5_523"></div>
-                <span class="v5_524">SPE ITS Student Chapter has earned the Outstanding Student Chapter Award for 2018,  The Outstanding Student Chapter Award is the highest honor a chapter can receive. In  recognition of its exceptional programs in Industry Engagement, Operations and  Planning, Community Involvement, Professional Development and Innovation</span>
-                <span class="v5_525">SPE Outstanding Student Chapter (OSC) Award 2018 , SPE International.</span>
-                <div class="name"></div>
-              </div>
-
-              <div class="v5_545">
-                <div class="v5_527"></div>
-                <div class="v5_528"></div>
-                <div class="v5_531"></div>
-                <div class="v5_532"></div>
-                <span class="v5_533">SPE ITS Student Chapter has earned the Excellence Student Chapter Award for 2020, in  Recognition of its exceptional programs in Industry Engagement, Operations and  Planning, Communinty Involvement, Professional Development and Innovation</span>
-                <span class="v5_534">SPE Excellence Student Chapter Award 2019, 2020,2021  SPE International.</span>
-                <div class="name"></div>
-              </div>
-            </div>
-            --}}
-    
     
 
 @endsection
