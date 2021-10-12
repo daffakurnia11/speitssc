b051bf504040de2ef730f21e382ea7846549ac1b
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2021-10-12 14:46:43 +0700

    Adding Vendor on Template Page

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}?modified={{ date("Ymd")}}">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{ asset('vendor/owlcarousel/dist/assets/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/owlcarousel/dist/assets/owl.theme.default.min.css') }}">
     {{-- Favicon --}}
     <link rel="shortcut icon" href="{{ asset('img/logo.png') }}" type="image/x-icon">
 
@@ -29,7 +32,19 @@
     <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
     <!-- jQuery -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <!-- Owl Carousel -->
+    <script src="{{ asset('vendor/owlcarousel/dist/owl.carousel.min.js') }}"></script>
     <script>
+      $(document).ready(function () {
+        $(".owl-carousel").owlCarousel({
+          loop: true,
+          margin: 10,
+          items: 1,
+          autoplay: true,
+          autoplayTimeout: 2000,
+          autoplayHoverPause: true
+        });
+      });
       $('#scroll-to-top').on('click', function() {
         $(window).scrollTop(0);
       });
