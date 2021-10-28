7e8cde7f0aeb750c51432d03c2806fcfd659d5ab
afanfian <afanfian14@gmail.com>
2021-10-28 16:39:00 +0700

    Pembuatan file baru Our Members

---
+++ resources/views/main/our_members.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.main')
+
+@section('navbar')
+  @include('templates.navbar')
+@endsection
+
+@section('footer')
+  @include('templates.footer')
+@endsection
+
+@section('title', 'Our Members')
